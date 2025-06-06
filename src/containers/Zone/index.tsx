be1fd37c81285343d30e83add74c8467e6c4acfd
HimaneeshYadala <himaneeshyadala@gmail.com>
2025-06-06 05:11:49 +0000

    Zone container conflict

--- src/containers/Zone/index.tsx
+++ src/containers/Zone/index.tsx
@@ -1,71 +1,3 @@
-<<<<<<< HEAD
-'use client';
-
-import React from 'react';
-import { Zones } from '@/components';
-import { useZones } from '@/hooks/useZones';
-import { useRouter } from 'next/navigation';
-
-const ZoneContainer: React.FC = () => {
-  const router = useRouter();
-  const {
-    // Data
-    zones,
-    stats,
-    loading,
-    error,
-    
-    // Actions
-    handleClearError,
-    handleUpdateZoneStatus,
-    
-    // Utility functions
-    getZoneById,
-    getZonesByStatus,
-    getTopPerformingZones,
-    getZonesNeedingAttention,
-    calculateZoneProgress,
-    getPerformanceIndicator,
-    getStatusColor,
-    getEfficiencyColor,
-    filterZones,
-    sortZones,
-  } = useZones();
-
-  const handleZoneClick = (zoneId: string) => {
-    router.push(`/zones/${zoneId}`);
-  };
-
-  return (
-    <Zones
-      // Data props
-      zones={zones}
-      stats={stats}
-      loading={loading}
-      error={error}
-      
-      // Action props
-      onClearError={handleClearError}
-      onUpdateZoneStatus={handleUpdateZoneStatus}
-      onZoneClick={handleZoneClick}
-      
-      // Utility props
-      getZoneById={getZoneById}
-      getZonesByStatus={getZonesByStatus}
-      getTopPerformingZones={getTopPerformingZones}
-      getZonesNeedingAttention={getZonesNeedingAttention}
-      calculateZoneProgress={calculateZoneProgress}
-      getPerformanceIndicator={getPerformanceIndicator}
-      getStatusColor={getStatusColor}
-      getEfficiencyColor={getEfficiencyColor}
-      filterZones={filterZones}
-      sortZones={sortZones}
-    />
-  );
-};
-
-export default ZoneContainer; 
-=======
 'use client';
 
 import React from 'react';
@@ -132,4 +64,3 @@ const ZoneContainer: React.FC = () => {
 };
 
 export default ZoneContainer; 
->>>>>>> master
